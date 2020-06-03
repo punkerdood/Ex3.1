e88d8802540e2eefb01bf02b4e9e588e8824f68b
Punkerdood <majpatmcd@gmail.com>
2020-06-03 13:37:54 -0400

    next

---
+++ Ex3.7,3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Ex3._7_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            
+            List<int> even = new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                    if(i%2 ==0)
+                {
+                    even.Add(i);
+                   
+                }
+
+            }
+            foreach (int steven in even)
+            {
+                sum += steven;
+            }
+            Console.WriteLine(sum);
+        }
+
+    }
+}
+        
+    
+
