d7beaa3ae543bb804ae3fef236e15106355429f5
Punkerdood <majpatmcd@gmail.com>
2020-06-03 12:52:26 -0400

    What a country!

---
+++ Ex3.2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Ex3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string drs = ("I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.");
+            string[] sep = drs.Split(".");
+            Console.WriteLine(string.Join("\n",sep));
+        }
+    }
+}
---
+++ Ex3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] someInts = { 1, 1, 2, 3, 5, 8 };
+            foreach (int num in someInts)
+            {
+                if (num%2 != 0) {
+                    Console.WriteLine(num); }
+            }
+           
+        }
+    }
+}
